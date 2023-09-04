bab722a543e5eb160f133ffd8af0d42f393fa83b
nuhptr <nugrahaadi733@gmail.com>
2023-09-04 15:45:22 +0700

    update meta page

--- pages/posts/[postId].tsx
+++ pages/posts/[postId].tsx
@@ -1,31 +1,38 @@
-import { useRouter } from 'next/router';
-import { ClipLoader } from 'react-spinners';
+import { useRouter } from 'next/router'
+import { ClipLoader } from 'react-spinners'
 
-import usePost from '@/hooks/usePost';
+import usePost from '@/hooks/usePost'
 
-import Header from '@/components/Header';
-import Form from '@/components/Form';
-import PostItem from '@/components/posts/PostItem';
-import CommentFeed from '@/components/posts/CommentFeed';
+import Header from '@/components/Header'
+import Form from '@/components/Form'
+import PostItem from '@/components/posts/PostItem'
+import CommentFeed from '@/components/posts/CommentFeed'
+import Head from 'next/head'
 
 const PostView = () => {
-  const router = useRouter();
-  const { postId } = router.query;
+  const router = useRouter()
+  const { postId } = router.query
 
-  const { data: fetchedPost, isLoading } = usePost(
-    postId as string
-  );
+  const { data: fetchedPost, isLoading } = usePost(postId as string)
 
   if (isLoading || !fetchedPost) {
     return (
       <div className='flex items-center justify-center h-full'>
         <ClipLoader color='lightblue' size={80} />
       </div>
-    );
+    )
   }
 
   return (
     <>
+      <Head>
+        <title>Twitter | {fetchedPost?.comments}</title>
+        <meta
+          name='description'
+          content='Twitter for sharing events each other'
+        />
+      </Head>
+
       <Header showBackArrow label='Tweet' />
       <PostItem data={fetchedPost} />
       <Form
@@ -35,7 +42,7 @@ const PostView = () => {
       />
       <CommentFeed comments={fetchedPost?.comments} />
     </>
-  );
-};
+  )
+}
 
-export default PostView;
+export default PostView
--- pages/users/[userId].tsx
+++ pages/users/[userId].tsx
@@ -1,37 +1,44 @@
-import { useRouter } from 'next/router';
-import { ClipLoader } from 'react-spinners';
+import { useRouter } from 'next/router'
+import { ClipLoader } from 'react-spinners'
 
-import useUser from '@/hooks/useUser';
+import useUser from '@/hooks/useUser'
 
-import PostFeed from '@/components/posts/PostFeed';
-import Header from '@/components/Header';
-import UserBio from '@/components/users/UserBio';
-import UserHero from '@/components/users/UserHero';
+import PostFeed from '@/components/posts/PostFeed'
+import Header from '@/components/Header'
+import UserBio from '@/components/users/UserBio'
+import UserHero from '@/components/users/UserHero'
+import Head from 'next/head'
 
 const UserView = () => {
-  const router = useRouter();
-  const { userId } = router.query;
+  const router = useRouter()
+  const { userId } = router.query
 
-  const { data: fetchedUser, isLoading } = useUser(
-    userId as string
-  );
+  const { data: fetchedUser, isLoading } = useUser(userId as string)
 
   if (isLoading || !fetchedUser) {
     return (
       <div className='flex items-center justify-center h-full'>
         <ClipLoader color='lightblue' size={80} />
       </div>
-    );
+    )
   }
 
   return (
     <>
+      <Head>
+        <title>Home | {fetchedUser?.name}</title>
+        <meta
+          name='description'
+          content='Twitter for sharing events each other'
+        />
+      </Head>
+
       <Header showBackArrow label={fetchedUser?.name} />
       <UserHero userId={userId as string} />
       <UserBio userId={userId as string} />
       <PostFeed userId={userId as string} />
     </>
-  );
-};
+  )
+}
 
-export default UserView;
+export default UserView
