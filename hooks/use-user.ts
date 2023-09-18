ba54400689fb68c735a8d114e9a00c78113b4771
Adi Nugraha Putra <nugrahaadi733@gmail.com>
2023-09-18 15:40:51 +0700

    hooks: clean up code

---
+++ hooks/use-user.ts
@@ -0,0 +1,9 @@
+import useSWR from 'swr'
+
+import fetcher from '../libs/fetcher'
+
+export default function useUser(userId: string) {
+  const { data, error, isLoading, mutate } = useSWR(userId ? `/api/users/${userId}` : null, fetcher)
+
+  return { data, error, isLoading, mutate }
+}
---
+++ hooks/use-users.ts
@@ -0,0 +1,9 @@
+import useSWR from 'swr'
+
+import fetcher from '@/libs/fetcher'
+
+export default function useUsers() {
+  const { data, error, isLoading, mutate } = useSWR('/api/users', fetcher)
+
+  return { data, error, isLoading, mutate }
+}
